891e8abf85a153353c2f5d032ed13c9f372586ad
soyowp <soyowp@gmail.com>
2021-10-08 14:26:16 +0900

    211008

--- security_prj/src/main/java/org/ict/controller/SecurityController.java
+++ security_prj/src/main/java/org/ict/controller/SecurityController.java
@@ -2,29 +2,30 @@ package org.ict.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
 
-
+@Controller
 @Log4j
 @RequestMapping("/secu/*")
-@Controller
 public class SecurityController {
-	
+
 	@GetMapping("/all")
 	public void doAll() {
 		log.info("모든사람이 접속 가능");
 	}
-	
+
 	@GetMapping("/member")
 	public void doMember() {
 		log.info("멤버만 접속 가능");
 	}
-	
-	@GetMapping("/all")
+
+	@GetMapping("/admin")
 	public void doAdmin() {
 		log.info("운영자만 접속 가능");
 	}
+
 
 }
